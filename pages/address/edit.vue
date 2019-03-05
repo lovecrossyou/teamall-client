390561b699a78a26cba0e0e05fe0bb7cb5feb3a1
007 <evengeo@163.com>
2019-03-05 18:50:54 +0800

    add addressModule

---
+++ pages/address/edit.vue
@@ -0,0 +1,22 @@
+<template>
+	<view class="content">
+		
+	</view>
+</template>
+
+<script >
+	export default{
+		data(){
+			return {}
+		},
+		components:{
+			
+		}
+	}
+</script>
+
+<style lang="less">
+	.content{
+		
+	}
+</style>
