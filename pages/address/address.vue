3bb70e56d906ce698a94ab87d0864ab186132e68
love1234 <345841634@qq.com>
2019-03-06 11:00:02 +0800

    地址处理

--- pages/address/address.vue
+++ pages/address/address.vue
@@ -10,182 +10,200 @@
 						</view>
 						<view class="addr-detail">{{ address.detail }}</view>
 					</view>
-					<image v-bind:src="modifyIcon" @click="edit"></image>
+					<image v-bind:src="modifyIcon" @click="edit(address)"></image>
 				</view>
 			</block>
 		</view>
 
 		<view class="add-addr-button" @click="edit">
-			<image v-bind:src="addIcon" ></image>
+			<image v-bind:src="addIcon"></image>
 			<view class="text">添加地址</view>
 		</view>
 	</view>
 </template>
 
 <script>
-export default {
-	methods:{
-		edit(){
-			uni.navigateTo({
-				url:"edit"
-			})
-		}
-	},
-	data() {
-		return {
-			modifyIcon: '../../static/addr/addr_edit_icon.png',
-			addIcon: '../../static/addr/add_icon.png',
-			addressList: [
-				{
-					name: '花花1',
-					phone: 13683574555,
-					detail: '北京市西城区粮科大厦1'
-				},
+	import {
+		mapMutations,
+	} from "vuex";
 
-				{
-					name: '花花2',
-					phone: 13683574666,
-					detail: '北京市西城区粮科大厦2'
-				},
-				{
-					name: '花花2',
-					phone: 13683574666,
-					detail: '北京市西城区粮科大厦2'
-				},
-				{
-					name: '花花2',
-					phone: 13683574666,
-					detail: '北京市西城区粮科大厦2'
-				},
-				{
-					name: '花花2',
-					phone: 13683574666,
-					detail: '北京市西城区粮科大厦2'
-				},
-				{
-					name: '花花2',
-					phone: 13683574666,
-					detail: '北京市西城区粮科大厦2'
-				},
-				{
-					name: '花花2',
-					phone: 13683574666,
-					detail: '北京市西城区粮科大厦2'
-				},
-				{
-					name: '花花2',
-					phone: 13683574666,
-					detail: '北京市西城区粮科大厦2'
-				},
-				{
-					name: '花花2',
-					phone: 13683574666,
-					detail: '北京市西城区粮科大厦2'
-				},
-				{
-					name: '花花2',
-					phone: 13683574666,
-					detail: '北京市西城区粮科大厦2'
-				},
-				{
-					name: '花花2',
-					phone: 13683574666,
-					detail: '北京市西城区粮科大厦2'
-				},
-				{
-					name: '花花2',
-					phone: 13683574666,
-					detail: '北京市西城区粮科大厦2'
-				},
-				{
-					name: '花花2',
-					phone: 13683574666,
-					detail: '北京市西城区粮科大厦2'
-				},
-				{
-					name: '花花2',
-					phone: 13683574666,
-					detail: '北京市西城区粮科大厦2'
-				}
-			]
-		};
-	}
-};
+	export default {
+		methods: {
+			...mapMutations({
+				"saveEdit": "address/saveEdit"
+			}),
+			edit(address) {
+				this.saveEdit(address);
+				uni.navigateTo({
+					url: "edit"
+				})
+			}
+		},
+		data() {
+			return {
+				modifyIcon: '../../static/addr/addr_edit_icon.png',
+				addIcon: '../../static/addr/add_icon.png',
+				addressList: [{
+						name: '花花1',
+						phone: 13683574555,
+						detail: '北京市西城区粮科大厦1'
+					},
+
+					{
+						name: '花花2',
+						phone: 13683574666,
+						detail: '北京市西城区粮科大厦2'
+					},
+					{
+						name: '花花2',
+						phone: 13683574666,
+						detail: '北京市西城区粮科大厦2'
+					},
+					{
+						name: '花花2',
+						phone: 13683574666,
+						detail: '北京市西城区粮科大厦2'
+					},
+					{
+						name: '花花2',
+						phone: 13683574666,
+						detail: '北京市西城区粮科大厦2'
+					},
+					{
+						name: '花花2',
+						phone: 13683574666,
+						detail: '北京市西城区粮科大厦2'
+					},
+					{
+						name: '花花2',
+						phone: 13683574666,
+						detail: '北京市西城区粮科大厦2'
+					},
+					{
+						name: '花花2',
+						phone: 13683574666,
+						detail: '北京市西城区粮科大厦2'
+					},
+					{
+						name: '花花2',
+						phone: 13683574666,
+						detail: '北京市西城区粮科大厦2'
+					},
+					{
+						name: '花花2',
+						phone: 13683574666,
+						detail: '北京市西城区粮科大厦2'
+					},
+					{
+						name: '花花2',
+						phone: 13683574666,
+						detail: '北京市西城区粮科大厦2'
+					},
+					{
+						name: '花花2',
+						phone: 13683574666,
+						detail: '北京市西城区粮科大厦2'
+					},
+					{
+						name: '花花2',
+						phone: 13683574666,
+						detail: '北京市西城区粮科大厦2'
+					},
+					{
+						name: '花花2',
+						phone: 13683574666,
+						detail: '北京市西城区粮科大厦2'
+					}
+				]
+			};
+		}
+	};
 </script>
 
 <style lang="less">
-	@footerHeight:102upx;
-.content {
-	width: 100%;
-	padding: 0 0 0 0;
-	.addr-list {
-		padding-bottom: @footerHeight;
-		.addr-item {
-			background-color: white;
+	@footerHeight: 102upx;
+
+	.content {
+		width: 100%;
+		padding: 0 0 0 0;
+
+		.addr-list {
+			padding-bottom: @footerHeight;
+
+			.addr-item {
+				background-color: white;
+				display: flex;
+				flex-direction: row;
+				padding: 0 38upx 0 40upx;
+				box-sizing: border-box;
+				width: 100%;
+				justify-content: space-between;
+				align-items: center;
+				height: 172upx;
+				border-bottom: solid 1upx #eaeaea;
+
+				.addr-info {
+					display: flex;
+					flex-direction: column;
+
+					.addr-info-top {
+						display: flex;
+						flex-direction: row;
+
+						.addr-name {
+							font-size: 32upx;
+							font-family: PingFang-SC-Medium;
+							font-weight: 500;
+							color: rgba(31, 31, 31, 1);
+						}
+
+						.addr-phone {
+							margin-left: 48upx;
+							font-size: 32upx;
+							font-family: PingFang-SC-Medium;
+							font-weight: 500;
+							color: rgba(31, 31, 31, 1);
+						}
+					}
+
+					.addr-detail {
+						font-size: 24upx;
+						font-family: PingFang-SC-Regular;
+						font-weight: 400;
+						color: rgba(153, 153, 153, 1);
+					}
+				}
+
+				image {
+					width: 36upx;
+					height: 36upx;
+				}
+			}
+		}
+
+		.add-addr-button {
+			position: fixed;
+			bottom: 0;
 			display: flex;
 			flex-direction: row;
-			padding: 0 38upx 0 40upx;
-			box-sizing: border-box;
 			width: 100%;
-			justify-content: space-between;
+			height: 102upx;
+			background: rgba(255, 89, 75, 1);
 			align-items: center;
-			height: 172upx;
-			border-bottom: solid 1upx #eaeaea;
-			.addr-info {
-				display: flex;
-				flex-direction: column;
-				.addr-info-top {
-					display: flex;
-					flex-direction: row;
-					.addr-name {
-						font-size: 32upx;
-						font-family: PingFang-SC-Medium;
-						font-weight: 500;
-						color: rgba(31, 31, 31, 1);
-					}
-					.addr-phone {
-						margin-left: 48upx;
-						font-size: 32upx;
-						font-family: PingFang-SC-Medium;
-						font-weight: 500;
-						color: rgba(31, 31, 31, 1);
-					}
-				}
-				.addr-detail {
-					font-size: 24upx;
-					font-family: PingFang-SC-Regular;
-					font-weight: 400;
-					color: rgba(153, 153, 153, 1);
-				}
-			}
-			image {
-				width: 36upx;
-				height: 36upx;
-			}
-		}
-	}
+			justify-content: center;
 
-	.add-addr-button {
-		position: fixed;
-		bottom: 0;
-		display: flex;
-		flex-direction: row;
-		width: 100%;
-		height: 102upx;
-		background: rgba(255, 89, 75, 1);
-		align-items: center;
-		justify-content: center;
-		image {
-			width: 32upx;
-			height: 32upx;
-		}
-		.text {
-			margin-left: 16upx;
-			font-size: 32upx;
-			font-family: PingFang-SC-Medium;
-			font-weight: 500;
-			color: rgba(255, 255, 255, 1);
+			image {
+				width: 32upx;
+				height: 32upx;
+			}
+
+			.text {
+				margin-left: 16upx;
+				font-size: 32upx;
+				font-family: PingFang-SC-Medium;
+				font-weight: 500;
+				color: rgba(255, 255, 255, 1);
+			}
 		}
 	}
-}
 </style>
