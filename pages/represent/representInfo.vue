2500f8b9da6ed9d32f22e522fc9b0eb362739c62
meng028 <15701664107@163.com>
2019-03-08 10:20:57 +0800

    xx

---
+++ pages/represent/representInfo.vue
@@ -0,0 +1,24 @@
+<template>
+	<view class="represent_info_wrapper">
+		<!-- <div className={styles.endorsement_nav}>
+			<img onClick={()=> {
+			router.go(-1);
+			}}
+			src={nav_left_arrow}
+			className={styles.endorsement_nav_arrow}
+			/>
+			<div className={styles.endorsement_nav_title}>{"代言商品"}</div>
+			<div className={styles.endorsement_nav_right} />
+		</div> -->
+		
+	</view>
+</template>
+
+<script>
+	
+	
+</script>
+
+<style lang="less">
+
+</style>
