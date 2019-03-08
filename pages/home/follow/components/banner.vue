a600b1b6e9d35989b90a95c9ae5feb7ea9252233
coder-yzg <1736532282@qq.com>
2019-03-08 18:24:48 +0800

    关注动态

---
+++ pages/home/follow/components/banner.vue
@@ -0,0 +1,63 @@
+<template>
+	<view>
+		  <view class="uni-padding-wrap">
+            <view class="page-section swiper">
+                <view class="page-section-spacing">
+                    <swiper class="swiper" :indicator-dots="indicatorDots" :autoplay="autoplay" :interval="interval" :duration="duration">
+                        <block v-for="(item,index) in moments_banner" :key="index">
+							<swiper-item>
+							    <view class="swiper-item uni-bg-red">
+									<image :src="item"></image>
+								</view>
+							</swiper-item>
+						</block>
+                    </swiper>
+                </view>
+            </view>
+        </view>
+	</view>
+</template>
+
+<script>
+	export default {
+    data() {
+        return {
+            background: ['color1', 'color2', 'color3'],
+            indicatorDots: true,
+            autoplay: true,
+            interval: 2000,
+            duration: 500,
+			moments_banner:[
+				"../../../../static/moments/moments1.jpg",
+				"../../../../static/moments/moments2.jpg",
+				"../../../../static/moments/moments3.jpg",
+				"../../../../static/moments/moments4.jpg",
+				"../../../../static/moments/moments5.jpg"
+			]
+        }
+    },
+    methods: {
+        changeIndicatorDots(e) {
+            this.indicatorDots = !this.indicatorDots
+        },
+        changeAutoplay(e) {
+            this.autoplay = !this.autoplay
+        },
+        intervalChange(e) {
+            this.interval = e.target.value
+        },
+        durationChange(e) {
+            this.duration = e.target.value
+        }
+    }
+}
+</script>
+
+<style scoped>
+	
+	.swiper-item>image{
+		width: 100%;
+		height: 520upx;
+	}
+	
+</style>
